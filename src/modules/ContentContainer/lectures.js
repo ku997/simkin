3482d93270068fa61e346ee5d642910d9cac110e
Maxim Simkin <maksimsimkinv@gmail.com>
2022-01-31 14:43:25 +0300

    Исправляет структуру глав электронного учебник, добавляет главу Заключение

--- src/modules/ContentContainer/lectures.js
+++ src/modules/ContentContainer/lectures.js
@@ -1,17 +1,28 @@
 import { Introduction } from "../../content/lectures/Introduction";
 import { Lecture1 } from "../../content/lectures/Lecture1";
-import { Lecture10 } from "../../content/lectures/Lecture10";
-import { Lecture11 } from "../../content/lectures/Lecture11";
 import { Lecture12 } from "../../content/lectures/Lecture12";
-import { Lecture13 } from "../../content/lectures/Lecture13";
-import { Lecture2 } from "../../content/lectures/Lecture2";
-import { Lecture3 } from "../../content/lectures/Lecture3";
-import { Lecture4 } from "../../content/lectures/Lecture4";
-import { Lecture5 } from "../../content/lectures/Lecture5";
-import { Lecture6 } from "../../content/lectures/Lecture6";
-import { Lecture7 } from "../../content/lectures/Lecture7";
-import { Lecture8 } from "../../content/lectures/Lecture8";
-import { Lecture9 } from "../../content/lectures/Lecture9";
+import { Lecture2_0 } from "../../content/lectures/Lecture2_0";
+import { Lecture2_1 } from "../../content/lectures/Lecture2_1";
+import { Lecture3_0 } from "../../content/lectures/Lecture3_0";
+import { Lecture3_1 } from "../../content/lectures/Lecture3_1";
+import Lecture3_2 from "../../content/lectures/Lecture3_2";
+import { Lecture3_3 } from "../../content/lectures/Lecture3_3";
+import { Lecture4_1 } from "../../content/lectures/Lecture4_1";
+import { Lecture4_2 } from "../../content/lectures/Lecture4_2";
+import { Lecture4_3 } from "../../content/lectures/Lecture4_3";
+import { Lecture4_4 } from "../../content/lectures/Lecture4_4";
+import { Lecture5_1 } from "../../content/lectures/Lecture5_1";
+import { Lecture5_2 } from "../../content/lectures/Lecture5_2";
+import { Lecture5_3 } from "../../content/lectures/Lecture5_3";
+import { Lecture6_1 } from "../../content/lectures/Lecture6_1";
+import { Lecture6_2 } from "../../content/lectures/Lecture6_2";
+import { Lecture6_3 } from "../../content/lectures/Lecture6_3";
+import { Lecture6_4 } from "../../content/lectures/Lecture6_4";
+import { Lecture7_1 } from "../../content/lectures/Lecture7_1";
+import { Lecture7_2 } from "../../content/lectures/Lecture7_2";
+import { Lecture7_3 } from "../../content/lectures/Lecture7_3";
+import { Lecture7_4 } from "../../content/lectures/Lecture7_4";
+import { Epilogue } from "../../content/lectures/Epilogue";
 
 export const content = [
   {
@@ -28,62 +39,106 @@ export const content = [
   },
   {
     id: 1.2,
-    component: <Lecture2 />,
+    component: <Lecture2_0 />,
   },
   {
     id: 1.3,
-    component: <Lecture3 />,
+    component: <Lecture2_1 />,
   },
   {
-    id: 2,
-    component: <Lecture4 />,
+    id: 1.4,
+    component: <Lecture3_0 />,
+  },
+  {
+    id: 1.5,
+    component: <Lecture3_1 />,
+  },
+  {
+    id: 1.6,
+    component: <Lecture3_2 />,
+  },
+  {
+    id: 1.7,
+    component: <Lecture3_3 />,
+  },
+  {
+    id: 1.8,
+    component: <Lecture4_1 />,
+  },
+  {
+    id: 1.9,
+    component: <Lecture4_2 />,
   },
   {
     id: 2.1,
-    component: <Lecture4 />,
+    component: <Lecture4_3 />,
   },
   {
     id: 2.2,
-    component: <Lecture5 />,
+    component: <Lecture4_4 />,
   },
   {
-    id: 2.3,
-    component: <Lecture6 />,
+    id: 2.4,
+    component: <Lecture5_1 />,
   },
   {
-    id: 3,
-    component: <Lecture7 />,
+    id: 2.5,
+    component: <Lecture5_1 />,
+  },
+  {
+    id: 2.6,
+    component: <Lecture5_2 />,
+  },
+  {
+    id: 2.7,
+    component: <Lecture5_3 />,
+  },
+  {
+    id: 2.8,
+    component: <Lecture6_1 />,
+  },
+  {
+    id: 2.9,
+    component: <Lecture6_1 />,
+  },
+  {
+    id: 3.0,
+    component: <Lecture6_2 />,
   },
   {
     id: 3.1,
-    component: <Lecture7 />,
+    component: <Lecture6_3 />,
   },
   {
     id: 3.2,
-    component: <Lecture8 />,
+    component: <Lecture6_4 />,
   },
   {
     id: 3.3,
-    component: <Lecture9 />,
+    component: <Lecture7_1 />,
   },
   {
     id: 3.4,
-    component: <Lecture10 />,
+    component: <Lecture7_1 />,
   },
   {
-    id: 4,
-    component: <Lecture11 />,
+    id: 3.5,
+    component: <Lecture7_2 />,
   },
   {
-    id: 4.1,
-    component: <Lecture11 />,
+    id: 3.6,
+    component: <Lecture7_3 />,
   },
   {
-    id: 4.2,
+    id: 3.7,
+    component: <Lecture7_4 />,
+  },
+  {
+    id: 3.8,
     component: <Lecture12 />,
   },
   {
-    id: 4.3,
-    component: <Lecture13 />,
+    id: 3.9,
+    component: <Epilogue />,
   },
 ];
